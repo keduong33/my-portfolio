4027eaf2e655de2e5630eb46a557b75d86119a49
Ke Duong <kelyduong@gmail.com>
2024-01-03 00:44:39 +1100

    change style n text of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
-    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <footer className="footer p-10 bg-neutral text-neutral-content mt-auto">
       <aside>
         <p>
-          <span className="font-bold">Tech stacks</span>: Vite, Tailwind,
-          daisyUI & Netlify
+          <span className="font-bold">This webapp was built with</span>: Vite,
+          Tailwind, daisyUI & Netlify
         </p>
       </aside>
     </footer>
