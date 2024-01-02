8b2e33c69e19525e6428f5be15f5defee5470f89
Ke Duong <kelyduong@gmail.com>
2024-01-03 01:55:54 +1100

    dark theme added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,7 @@ export default {
   important: true,
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
