18da62aa480f3b5a4cc0d0bfd8cfe9c08eded932
Ke Duong <kelyduong@gmail.com>
2024-01-03 00:49:38 +1100

    make projects into a column when small screen

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,35 +4,37 @@ function Projects() {
       <div className="prose pb-2">
         <h2>Projects</h2>
       </div>
-      <div className="card card-compact max-w-[300px] bg-neutral shadow-xl">
-        <figure className="mb-0 px-4">
-          <img
-            src="src/assets/chatbot.png"
-            alt="chatbot"
-            className="max-w-[150px]"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title mt-0">Language Partner Chatbot</h2>
-          <p className="text-justify">
-            A friendly LLM bot that user can converse via text/speech to learn
-            English. Txhis partner will fix the user's grammars upon request.
-          </p>
-          <div className="card-actions ">
-            <a
-              className="link link-primary"
-              href="https://chattychatty.netlify.app/"
-              target="_blank"
-            >
-              Demo
-            </a>
-            <a
-              className="link link-secondary"
-              href="https://github.com/keduong33/ChattyChatty"
-              target="_blank"
-            >
-              GitHub
-            </a>
+      <div className="w-full flex lg:flex-row flex-col items-center">
+        <div className="card card-compact max-w-[300px] bg-neutral shadow-xl">
+          <figure className="mb-0 px-4">
+            <img
+              src="src/assets/chatbot.png"
+              alt="chatbot"
+              className="max-w-[150px]"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title mt-0">Language Partner Chatbot</h2>
+            <p className="text-justify">
+              A friendly LLM bot that user can converse via text/speech to learn
+              English. Txhis partner will fix the user's grammars upon request.
+            </p>
+            <div className="card-actions ">
+              <a
+                className="link link-primary"
+                href="https://chattychatty.netlify.app/"
+                target="_blank"
+              >
+                Demo
+              </a>
+              <a
+                className="link link-secondary"
+                href="https://github.com/keduong33/ChattyChatty"
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
         </div>
       </div>
