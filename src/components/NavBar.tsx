38ee29a09ce371d28a9f2d6e092f12c3839669bf
Ke Duong <kelyduong@gmail.com>
2024-01-03 00:44:19 +1100

    add logos and fix style of nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,28 @@
+import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
+
 const NavBarOptions = () => (
   <>
     <li>
-      <a>Experiences</a>
+      <a href="#experiences">Experiences</a>
     </li>
     <li>
-      <a>Projects</a>
+      <a href="#projects">Projects</a>
     </li>
     <li>
       <details>
         <summary>Socials</summary>
-        <ul className="p-2">
+        <ul className="px-2">
           <li>
-            <a>LinkedIn</a>
+            <a href="https://www.linkedin.com/in/ke-duong/" target="_blank">
+              <LinkedinLogo />
+              LinkedIn
+            </a>
           </li>
           <li>
-            <a>GitHub</a>
+            <a href="https://github.com/keduong33" target="_blank">
+              <GithubLogo />
+              GitHub
+            </a>
           </li>
         </ul>
       </details>
@@ -22,12 +30,12 @@ const NavBarOptions = () => (
     <li>
       <details>
         <summary>Misc</summary>
-        <ul className="p-2">
+        <ul className="px-2">
           <li>
-            <a>Reading List</a>
+            <a href="#readings">Readings</a>
           </li>
           <li>
-            <a>Favorite Quotes</a>
+            <a href="#quotes"> Quotes</a>
           </li>
         </ul>
       </details>
@@ -37,7 +45,7 @@ const NavBarOptions = () => (
 
 function NavBar() {
   return (
-    <div className="sticky navbar bg-base-100 justify-between top-0 p-0">
+    <div className="sticky navbar bg-base-100 justify-between top-0 p-0 px-4 z-10">
       <div className="justify-start">
         <div className="dropdown md:hidden">
           <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
@@ -65,12 +73,14 @@ function NavBar() {
         </div>
         <div className="prose">
           <h1>
-            <a className="link link-hover">Ke</a>
+            <a className="link link-hover" href="#home">
+              Ke Duong
+            </a>
           </h1>
         </div>
       </div>
-      <div className="justify-end hidden md:flex lg:pr-4 pr-2">
-        <ul className="menu menu-horizontal lg:menu-lg gap-5">
+      <div className="justify-end hidden md:block lg:pr-4 pr-2">
+        <ul className="menu menu-horizontal lg:menu-lg gap-10">
           <NavBarOptions />
         </ul>
       </div>
