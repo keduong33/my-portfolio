d7aee4c2f867c07c4e4afe634b91df59e78e3f5b
Ke Duong <kelyduong@gmail.com>
2024-01-03 00:15:59 +1100

    add project section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,43 @@
+function Projects() {
+  return (
+    <div id="projects">
+      <div className="prose pb-2">
+        <h2>Projects</h2>
+      </div>
+      <div className="card card-compact max-w-[300px] bg-neutral shadow-xl">
+        <figure className="mb-0 px-4">
+          <img
+            src="src/assets/chatbot.png"
+            alt="chatbot"
+            className="max-w-[150px]"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title mt-0">Language Partner Chatbot</h2>
+          <p className="text-justify">
+            A friendly LLM bot that user can converse via text/speech to learn
+            English. Txhis partner will fix the user's grammars upon request.
+          </p>
+          <div className="card-actions ">
+            <a
+              className="link link-primary"
+              href="https://chattychatty.netlify.app/"
+              target="_blank"
+            >
+              Demo
+            </a>
+            <a
+              className="link link-secondary"
+              href="https://github.com/keduong33/ChattyChatty"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
