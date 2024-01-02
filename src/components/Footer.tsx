53a78af092cda584ffe8ecce49db3260799ab56d
Ke Duong <kelyduong@gmail.com>
2024-01-03 01:43:28 +1100

    update tailwindcss - tailwind css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="footer p-10 bg-neutral text-neutral-content mt-auto">
       <aside>
         <p>
-          <span className="font-bold">Built-using</span>: Vite, Tailwind &
+          <span className="font-bold">Built-using</span>: Vite, Tailwind CSS &
           daisyUI
           <br />
           <span className="font-bold">Hosted on</span>: Netlify
