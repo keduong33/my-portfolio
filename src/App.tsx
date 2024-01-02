c45601d68ebfe735f75f777779e403e22e98e7bf
Ke Duong <kelyduong@gmail.com>
2024-01-03 00:07:02 +1100

    bring navbar out

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
+import About from "./components/About";
 import Footer from "./components/Footer";
-import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="flex flex-col justify-between h-svh">
-      <div className="p-4">
-        <NavBar />
-        <Home />
-      </div>
-
+    <div className="flex flex-col h-svh " id="home">
+      <NavBar />
+      <About />
       <Footer />
     </div>
   );
