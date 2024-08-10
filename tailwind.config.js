c5a717fe55d832614c772459cbd5f0f869c7bf59
Ke Duong <kelyduong@gmail.com>
2024-08-10 23:45:25 +1000

    Delete everything

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  important: true,
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: ["dark"],
-  },
-};
