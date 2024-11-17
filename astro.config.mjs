d18e5d8983135577be3b7a5a44ae586f91ccefe2
Ke Duong <kelyduong@gmail.com>
2024-11-17 22:24:25 +1100

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
+  integrations: [tailwind({
     applyBaseStyles: false,
   }),]
 });
